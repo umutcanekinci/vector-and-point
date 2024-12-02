7e14a3b73d6722ddcce22b6fff9f84d4f360ba44
umutcanekinci <muetnmuetn@gmail.com>
2024-12-03 00:33:49 +0300

    I have added constant keyword to functions and arguments. I have added Cross Multiplication function to vector class. .gitignore is created.

--- main.cpp
+++ main.cpp
@@ -3,93 +3,96 @@ using namespace std;
 
 class Point {
 public:
-    float x, y;
-    Point(float x=0, float y=0);
+
+    /* Other way to declare the constructor especially when the constructor is more complex
+
+    Point(double x=0, double y=0);
+
+    Point::Point(double x, double y) {
+        this -> x = x;
+        this -> y = y;
+    }
+    */
+    double x, y;
+    Point(double x=0, double y=0) : x(x), y(y) {}
     void printCoordinates();
+
 };
 
 class Vector : public Point {
 public:
+
+    /* Constant Member Functions and Constant Arguments
+        Constant member functions are member functions that do not modify the object they are called on and its members.
+        They are declared by adding the const keyword at the end of the function declaration.
+
+        Constant arguments are arguments that are passed to a function as constant references.
+        They are used to prevent the function from modifying the argument.
+        They are declared by adding the const keyword before the type of the argument.
+
+    */
+
     using Point::Point;
-    Vector operator+(Vector vec);
-    Vector operator-(Vector vec);
-    Vector operator*(int scalar);
-    Vector operator/(int scalar);
-    float magnitude();
-    float magnitudeSquared();
-    Vector normalize();
-};
 
+    double magnitude() const;
+    double magnitudeSquared() const;
 
+    Vector normalized() const;
+    Vector Cross(const Vector &vector) const;
 
+    Vector operator+(const Vector &vector) const; 
+    Vector operator-(const Vector &vector) const;
+    Vector operator*(double scalar) const;
+    Vector operator/(double scalar) const;
 
-Point::Point(float x, float y) {
-    this -> x = x;
-    this -> y = y;
 };
 
 void Point::printCoordinates() {
     cout << "(" << x << ", " << y << ")" << endl;
+}
+
+double Vector::magnitude() const {
+    return sqrt(magnitudeSquared());
+}
+
+double Vector::magnitudeSquared() const {
+    return x * x + y * y;
+}
+
+Vector Vector::normalized() const{
+    return Vector(x / magnitude(), y / magnitude());
+}
+
+Vector Vector::Cross(const Vector &vector) const {
+    return Vector(x * vector.y - y * vector.x);
+}
+
+#pragma region Operators
+
+Vector Vector::operator+(const Vector &vector) const {
+    return Vector(x + vector.x, y + vector.y);
 };
 
-Vector Vector::operator+(Vector vec) {
-    Vector result;
-    result.x = x + vec.x;
-    result.y = y + vec.y;
-    return result;
+Vector Vector::operator-(const Vector &vector) const {
+    return Vector(x - vector.x, y - vector.y);
 };
 
-Vector Vector::operator-(Vector vec) {
-    Vector result;
-    result.x = x - vec.x;
-    result.y = y - vec.y;
-    return result;
+Vector Vector::operator*(double scalar) const {
+    return Vector(x * scalar, y * scalar);
+}
+
+Vector Vector::operator/(double scalar) const {
+    return Vector(x / scalar, y / scalar);
 };
 
-Vector Vector::operator*(int scalar) {
-    Vector result;
-    result.x = x * scalar;
-    result.y = y * scalar;
-    return result;
-};
+#pragma endregion
 
-Vector Vector::operator/(int scalar) {
-    Vector result;
-    result.x = x / scalar;
-    result.y = y / scalar;
-    return result;
-};
-
-float Vector::magnitude() {
-    return sqrt(x*x + y*y);
-};
-
-float Vector::magnitudeSquared() {
-    return x*x + y*y;
-};
-
-Vector Vector::normalize() {
-    float mag = magnitude();
-    Vector result;
-    result.x = x / mag;
-    result.y = y / mag;
-    return result;
-};
-
-
-
-
-
-
-
-
-
-int main() {
+void test() {
 
     int movementSpeed = 5;
 
     Vector position = Vector(1, 2);
-    Vector velocity = Vector(3, 4).normalize() * movementSpeed;
+    Vector velocity = Vector(3, 4).normalized() * movementSpeed;
     Vector friction = Vector(0.1, 0.1);
 
     position.printCoordinates();
@@ -104,6 +107,11 @@ int main() {
 
     cout << "Magnitude: " << position.magnitude() << endl;
 
+}
+
+int main() {
+
+    test();
     return 0;
 
-};
+}
